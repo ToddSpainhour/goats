43322afe74d0f1d41cc7209cb103b486788e8f74
Todd Spainhour <toddspainhour@gmail.com>
2020-05-12 18:34:12 -0500

    button wip

--- src/components/Goat/Goat.js
+++ src/components/Goat/Goat.js
@@ -9,24 +9,31 @@ class Goat extends React.Component {
     useAGoat(goat.id);
   }
 
-  render() {
-    const { goat } = this.props;
+freeGoatEvent = (e) => {
+  e.preventDefault();
+  const { goat, freeAGoat } = this.props;
+  freeAGoat(goat.id);
+}
 
-    return (
-      <div className="Goat col-3">
-        <div className="card">
-          <img className="card-img-top" src={goat.imgUrl} alt="Goat Card"/>
-          <div className="card-body">
-            <h5 className="card-title">{goat.name}</h5>
-            <p className="card-text">Beard Length: {goat.beardLength}</p>
-          </div>
-          <div className="card-footer">
-            <button className="btn btn-dark" onClick={this.useGoatEvent}>Use this goat</button>
-          </div>
+render() {
+  const { goat } = this.props;
+
+  return (
+    <div className="Goat col-3">
+      <div className="card">
+        <img className="card-img-top" src={goat.imgUrl} alt="Goat Card"/>
+        <div className="card-body">
+          <h5 className="card-title">{goat.name}</h5>
+          <p className="card-text">Beard Length: {goat.beardLength}</p>
+        </div>
+        <div className="card-footer">
+          <button className="btn btn-dark" onClick={this.useGoatEvent}>Use this goat</button>
+          <button className="btn btn-danger" onClick={this.freeGoatEvent}>Free This goat</button>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
+}
 }
 
 export default Goat;
